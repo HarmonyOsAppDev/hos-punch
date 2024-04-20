4a99665ab961f0cdd611c0550c9d7405945d03ae
dubinbin <szdubinbin@gmail.com>
2024-04-21 02:29:30 +0800

    feat: 优化ui和图标

--- entry/src/main/ets/statics/index.ts
+++ entry/src/main/ets/statics/index.ts
@@ -33,78 +33,86 @@ export const BottomText = {
 export const Icon_list = [
   {
     id: "1",
-    url: "app.media.tabicon_hanbaokuaican"
+    url: "app.media.awo"
   },
   {
     id: "2",
-    url: "app.media.tabicon_jituizhaji"
+    url: "app.media.bianji"
   },
   {
     id: "3",
-    url: "app.media.tabicon_kafei"
+    url: "app.media.paizhao"
   },
   {
     id: "4",
-    url: "app.media.tabicon_maocaichuanchuan"
+    url: "app.media.pinglun"
   },
   {
     id: "5",
-    url: "app.media.tabicon_mianbao"
+    url: "app.media.chakansousuo"
   },
   {
     id: "6",
-    url: "app.media.tabicon_mianbaotusi"
+    url: "app.media.chongwu"
   },
   {
     id: "7",
-    url: "app.media.tabicon_mifenmiantiao"
+    url: "app.media.cunchu"
   },
   {
     id: "8",
-    url: "app.media.tabicon_naichaguozhi"
+    url: "app.media.cuxiao"
   },
   {
     id: "9",
-    url: "app.media.tabicon_naichanaigai"
+    url: "app.media.daka"
   },
   {
     id: "10",
-    url: "app.media.tabicon_naiyoudangao"
+    url: "app.media.dangan"
   },
   {
     id: "11",
-    url: "app.media.tabicon_pijiujingniang"
+    url: "app.media.dingwei"
   },
   {
     id: "12",
-    url: "app.media.tabicon_pisaxican"
+    url: "app.media.ershouche"
   },
   {
     id: "13",
-    url: "app.media.tabicon_shaguobaozifan"
+    url: "app.media.fujin"
   },
   {
     id: "14",
-    url: "app.media.tabicon_shalaqingshi"
+    url: "app.media.gongwenbao"
   },
   {
     id: "15",
-    url: "app.media.tabicon_shaokaochuanchuan"
+    url: "app.media.guanjun"
   },
   {
     id: "16",
-    url: "app.media.tabicon_shousifantuan"
+    url: "app.media.hongbao"
   },
   {
     id: "17",
-    url: "app.media.tabicon_xianhualvzhi"
+    url: "app.media.huiyuan"
   },
   {
     id: "18",
-    url: "app.media.tabicon_xuegaobingbang"
+    url: "app.media.huodong"
   },
   {
     id: "19",
-    url: "app.media.tabicon_zhongcanchaocai"
+    url: "app.media.jifen"
+  },
+  {
+    id: "20",
+    url: "app.media.kabao"
+  },
+  {
+    id: "21",
+    url: "app.media.daishouhuo"
   }
 ]
